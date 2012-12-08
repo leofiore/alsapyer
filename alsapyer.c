f215edbcca229fb1ea1b1fd070350fa794f49f74
leonardo <leonardo@debian>
2012-12-08 23:18:16 +0100

    fix and add license

--- alsapyer.c
+++ alsapyer.c
@@ -1,3 +1,25 @@
+/* alsapyer.c
+ * 
+ * A ridicolousy simple Python binding for libalsaplayer
+ *
+ * Author: Leonardo
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *                    Version 2, December 2004
+ *
+ * Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+ *
+ * Everyone is permitted to copy and distribute verbatim or modified
+ * copies of this license document, and changing it is allowed as long
+ * as the name is changed.
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *  0. You just DO WHAT THE FUCK YOU WANT TO.
+ *
+ */
+
 #include <Python.h>
 #include <alsaplayer/control.h>
 
@@ -248,6 +270,11 @@ alsapyer_set_speed(PyObject *self, PyObject *args){
     if (!PyArg_ParseTuple(args, "if", &session, &speed))
         return NULL;
 
+    if (speed > 4)
+        speed = 4;
+    else if ( speed < -4)
+        speed = -4;
+
     result = ap_set_speed(session, speed);
 
     return Py_BuildValue("i", result);
@@ -276,6 +303,11 @@ alsapyer_set_volume(PyObject *self, PyObject *args){
     if (!PyArg_ParseTuple(args, "if", &session, &volume))
         return NULL;
 
+    if (volume > 1)
+        volume = 1;
+    else if (volume < 0)
+        volume = 0;
+
     result = ap_set_volume(session, volume);
 
     return Py_BuildValue("i", result);
@@ -303,6 +335,11 @@ alsapyer_set_pan(PyObject *self, PyObject *args){
 
     if (!PyArg_ParseTuple(args, "if", &session, &pan))
         return NULL;
+
+    if (pan > 1)
+        pan = 1;
+    else if (pan < -1)
+        pan = -1;
 
     result = ap_set_pan(session, pan);
 
@@ -576,14 +613,14 @@ alsapyer_set_position(PyObject *self, PyObject *args){
 static PyObject *
 alsapyer_get_position(PyObject *self, PyObject *args){
 
-    int session, *val, result;
+    int session, val, result;
 
     if (!PyArg_ParseTuple(args, "i", &session))
         return NULL;
 
-    result = ap_get_position(session, val);
+    result = ap_get_position(session, &val);
 
-    return Py_BuildValue("i", *val);
+    return Py_BuildValue("i", val);
 }
 
 static PyObject *
@@ -602,14 +639,14 @@ alsapyer_set_position_relative(PyObject *self, PyObject *args){
 static PyObject *
 alsapyer_get_length(PyObject *self, PyObject *args){
 
-    int session, *val, result;
+    int session, val, result;
 
     if (!PyArg_ParseTuple(args, "i", &session))
         return NULL;
 
-    result = ap_get_length(session, val);
+    result = ap_get_length(session, &val);
 
-    return Py_BuildValue("i", *val);
+    return Py_BuildValue("i", val);
 }
 
 #ifdef AP_CONTROL_VERSION
@@ -843,80 +880,99 @@ alsapyer_get_playlist(PyObject *self, PyObject *args){
 
 
 static PyMethodDef AlsapyerMethods[] = {
-    {"find_session", alsapyer_find_session, METH_VARARGS, "find session"},
-    {"session_running", alsapyer_session_running, METH_VARARGS, " "},
-    {"version", alsapyer_version, METH_VARARGS, " "},
+    {"find_session", alsapyer_find_session, METH_VARARGS, "Find a session"},
+    {"session_running", alsapyer_session_running, METH_VARARGS, "Check if a session is running"},
+    {"version", alsapyer_version, METH_VARARGS, "Get current version"},
 
-    {"play", alsapyer_play, METH_VARARGS, " "},
-    {"stop", alsapyer_stop, METH_VARARGS, " "},
-    {"pause", alsapyer_pause, METH_VARARGS, " "},
-    {"unpause", alsapyer_unpause, METH_VARARGS, " "},
-    {"next", alsapyer_next, METH_VARARGS, " "},
-    {"prev", alsapyer_prev, METH_VARARGS, " "},
-    {"ping", alsapyer_ping, METH_VARARGS, " "},
-    {"quit", alsapyer_quit, METH_VARARGS, " "},
-    {"clear_playlist", alsapyer_clear_playlist, METH_VARARGS, " "},
-    {"add_path", alsapyer_add_path, METH_VARARGS, " "},
-    {"add_and_play", alsapyer_add_and_play, METH_VARARGS, " "},
-    {"add_playlist", alsapyer_add_playlist, METH_VARARGS, " "},
-    {"shuffle_playlist", alsapyer_shuffle_playlist, METH_VARARGS, " "},
-    {"save_playlist", alsapyer_save_playlist, METH_VARARGS, " "},
-    {"get_playlist_length", alsapyer_get_playlist_length, METH_VARARGS, " "},
+    {"play", alsapyer_play, METH_VARARGS, "Play the current song"},
+    {"stop", alsapyer_stop, METH_VARARGS, "Stop the current song"},
+    {"pause", alsapyer_pause, METH_VARARGS, "Pause the current song"},
+    {"unpause", alsapyer_unpause, METH_VARARGS, "Continue to play the song"},
+    {"next", alsapyer_next, METH_VARARGS, "Move to the next track of the playlist"},
+    {"prev", alsapyer_prev, METH_VARARGS, "Move to the previous track of the playlist"},
+    {"ping", alsapyer_ping, METH_VARARGS, "Check if the player is responding"},
+    {"quit", alsapyer_quit, METH_VARARGS, "Close the alsaplayer session"},
+    {"clear_playlist", alsapyer_clear_playlist, METH_VARARGS, "Empty the playlist"},
+    {"add_path", alsapyer_add_path, METH_VARARGS, "Add a path to the playlist"},
+    {"add_and_play", alsapyer_add_and_play, METH_VARARGS, "Add a path to the playlist and plays it"},
+    {"add_playlist", alsapyer_add_playlist, METH_VARARGS, "Load a playlist into the current session"},
+    {"shuffle_playlist", alsapyer_shuffle_playlist, METH_VARARGS, "Shuffle the playlist"},
+    {"save_playlist", alsapyer_save_playlist, METH_VARARGS, "Save the current playlist"},
+    {"get_playlist_length", alsapyer_get_playlist_length, METH_VARARGS, "Get the total duration of the playlist"},
 
-    {"set_speed", alsapyer_set_speed, METH_VARARGS, " "},
-    {"get_speed", alsapyer_get_speed, METH_VARARGS, " "},
-    {"set_volume", alsapyer_set_volume, METH_VARARGS, " "},
-    {"get_volume", alsapyer_get_volume, METH_VARARGS, " "},
-    {"set_pan", alsapyer_set_pan, METH_VARARGS, " "},
-    {"get_pan", alsapyer_get_pan, METH_VARARGS, " "},
+    {"set_speed", alsapyer_set_speed, METH_VARARGS, "Set the speed of the current song (float value)"},
+    {"get_speed", alsapyer_get_speed, METH_VARARGS, "Get the speed of the current song"},
+    {"set_volume", alsapyer_set_volume, METH_VARARGS, "Set the volume of the player (0, ... ,1)"},
+    {"get_volume", alsapyer_get_volume, METH_VARARGS, "Get the volume of the player"},
+    {"set_pan", alsapyer_set_pan, METH_VARARGS, "Set the stereo balance of the player"},
+    {"get_pan", alsapyer_get_pan, METH_VARARGS, "Get the stereo balance of the player"},
 
 #ifdef AP_CONTROL_VERSION
-    {"is_paused", alsapyer_is_paused, METH_VARARGS, " "},
+    {"is_paused", alsapyer_is_paused, METH_VARARGS, "Check if the player is paused"},
 #endif
 
-    {"set_looping", alsapyer_set_looping, METH_VARARGS, " "},
-    {"is_looping", alsapyer_is_looping, METH_VARARGS, " "},
-    {"set_playlist_looping", alsapyer_set_playlist_looping, METH_VARARGS, " "},
-    {"is_playlist_looping", alsapyer_is_playlist_looping, METH_VARARGS, " "},
+    {"set_looping", alsapyer_set_looping, METH_VARARGS, "Enable Song looping"},
+    {"is_looping", alsapyer_is_looping, METH_VARARGS, "Check if the song is looping"},
+    {"set_playlist_looping", alsapyer_set_playlist_looping, METH_VARARGS, "Enable Playlist looping"},
+    {"is_playlist_looping", alsapyer_is_playlist_looping, METH_VARARGS, "Check if playlist is looping"},
 
-    {"get_tracks", alsapyer_get_tracks, METH_VARARGS, " "},
+    {"get_tracks", alsapyer_get_tracks, METH_VARARGS, "Get the tracklist"},
 
-    {"get_session_name", alsapyer_get_session_name, METH_VARARGS, " "},
+    {"get_session_name", alsapyer_get_session_name, METH_VARARGS, "Get the name of selected session"},
 
-    {"get_title", alsapyer_get_title, METH_VARARGS, " "},
-    {"get_artist", alsapyer_get_artist, METH_VARARGS, " "},
-    {"get_album", alsapyer_get_album, METH_VARARGS, " "},
-    {"get_genre", alsapyer_get_genre, METH_VARARGS, " "},
-    {"get_year", alsapyer_get_year, METH_VARARGS, " "},
-    {"get_track_number", alsapyer_get_track_number, METH_VARARGS, " "},
-    {"get_comment", alsapyer_get_comment, METH_VARARGS, " "},
-    {"get_file_path", alsapyer_get_file_path, METH_VARARGS, " "},
+    {"get_title", alsapyer_get_title, METH_VARARGS, "Get current song's title"},
+    {"get_artist", alsapyer_get_artist, METH_VARARGS, "Get current song's artist"},
+    {"get_album", alsapyer_get_album, METH_VARARGS, "Get current song's album"},
+    {"get_genre", alsapyer_get_genre, METH_VARARGS, "Get current song's genre"},
+    {"get_year", alsapyer_get_year, METH_VARARGS, "Get current song's year"},
+    {"get_track_number", alsapyer_get_track_number, METH_VARARGS, "Get current song's track position in the album"},
+    {"get_comment", alsapyer_get_comment, METH_VARARGS, "Get current songs'"},
+    {"get_file_path", alsapyer_get_file_path, METH_VARARGS, "Get current song's path in the filesystem"},
     
-    {"set_position", alsapyer_set_position, METH_VARARGS, " "},
-    {"get_position", alsapyer_get_position, METH_VARARGS, " "},
-    {"set_position_relative", alsapyer_set_position_relative, METH_VARARGS, " "},
-    {"get_length", alsapyer_get_length, METH_VARARGS, " "},
+    {"set_position", alsapyer_set_position, METH_VARARGS, "Set the cursor position in the track"},
+    {"get_position", alsapyer_get_position, METH_VARARGS, "Get the cursor position in the track"},
+    {"set_position_relative", alsapyer_set_position_relative, METH_VARARGS, "Jump in the track from the current position given an offset"},
+    {"get_length", alsapyer_get_length, METH_VARARGS, "Get length"},
 #ifdef AP_CONTROL_VERSION
     {"set_block", alsapyer_set_block, METH_VARARGS, " "},
     {"get_block", alsapyer_get_block, METH_VARARGS, " "},
     {"get_blocks", alsapyer_get_blocks, METH_VARARGS, " "},
 #endif
 
-    {"get_stream_type", alsapyer_get_stream_type, METH_VARARGS, " "},
-    {"get_status", alsapyer_get_status, METH_VARARGS, " "},
+    {"get_stream_type", alsapyer_get_stream_type, METH_VARARGS, "Get stream type"},
+    {"get_status", alsapyer_get_status, METH_VARARGS, "Get player status"},
 
-    {"is_playing", alsapyer_is_playing, METH_VARARGS, " "},
+    {"is_playing", alsapyer_is_playing, METH_VARARGS, "Check if the player is playng"},
 
-    {"sort", alsapyer_sort, METH_VARARGS, " "},
-    {"jump_to", alsapyer_jump_to, METH_VARARGS, " "},
+    {"sort", alsapyer_sort, METH_VARARGS, "Sort the playlist\n\n\
+         a - sort by artist in descending direction,\n\
+         A - sort by astist in ascending direction,\n\
+         l - sort by album in descending direction,\n\
+         L - sort by album in ascending direction,\n\
+         t - sort by title in descending direction,\n\
+         T - sort by title in ascending direction,\n\
+         y - sort by year in descending direction,\n\
+         Y - sort by year in ascending direction,\n\
+         n - sort by track number in descending direction,\n\
+         N - sort by track number in ascending direction,\n\
+         c - sort by comment in descending direction,\n\
+         C - sort by comment in ascending direction,\n\
+         f - sort by filename in descending direction,\n\
+         F - sort by filename in ascending direction,\n\
+         g - sort by genre in descending direction,\n\
+         G - sort by genre in ascending direction,\n\
+         p - sort by playtime in descending direction,\n\
+         P - sort by playtime in ascending direction.\n\
+        "},
+    {"jump_to", alsapyer_jump_to, METH_VARARGS, "Jump to a song"},
 
-    {"get_playlist_position", alsapyer_get_playlist_position, METH_VARARGS, " "},
-    {"get_file_path_for_track", alsapyer_get_file_path_for_track, METH_VARARGS, " "},
-    {"insert", alsapyer_insert, METH_VARARGS, " "},
-    {"remove", alsapyer_remove, METH_VARARGS, " "},
-    {"set_current", alsapyer_set_current, METH_VARARGS, " "},
+    {"get_playlist_position", alsapyer_get_playlist_position, METH_VARARGS, "Get current playlist position"},
+    {"get_file_path_for_track", alsapyer_get_file_path_for_track, METH_VARARGS, "Get file path for the current track"},
+    {"insert", alsapyer_insert, METH_VARARGS, "Insert in the playlist"},
+    {"remove", alsapyer_remove, METH_VARARGS, "Remove from the playlist"},
+    {"set_current", alsapyer_set_current, METH_VARARGS, "Set a track as the current"},
 
-    {"get_playlist", alsapyer_get_playlist, METH_VARARGS, " "},
+    {"get_playlist", alsapyer_get_playlist, METH_VARARGS, "Get the playlist"},
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
